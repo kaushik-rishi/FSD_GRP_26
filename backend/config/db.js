6a75e858a0d467b7c9a6f89afbd25a7045bf88f5
Oruganti Chetan Reddy <56594923+chetan-1924@users.noreply.github.com>
2021-12-14 23:18:55 +0530

    mid update

---
+++ backend/config/db.js
@@ -0,0 +1,22 @@
+import mongose from "mongoose";
+import colors from "colors";
+
+// a mongoose stuf (mongoose.connect ....) return always a promise
+const connectDB = async () => {
+	try {
+		const conn = await mongose.connect(
+			"mongodb+srv://kaushikrishi:Kamal_123@cluster0.pjncu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+			{
+				useUnifiedTopology: true,
+				useNewUrlParser: true,
+				useCreateIndex: true,
+			}
+		);
+		console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+	} catch (error) {
+		console.error(`Error: ${error.message}`.red.underline.bold);
+		process.exit(1);
+	}
+};
+
+export default connectDB;
