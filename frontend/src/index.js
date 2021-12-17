6aec8ba5d0609dd59f432e0df3d861b4dd4ddb49
Oruganti Chetan Reddy <56594923+chetan-1924@users.noreply.github.com>
2021-12-17 21:24:56 +0530

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './App.css';
-import {Provider} from 'react-redux'
-import store from './store'
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import { Provider } from "react-redux";
+import store from "./store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
