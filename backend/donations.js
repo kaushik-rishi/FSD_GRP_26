18d1711ea1ee2a09671ef9a09a5a4e7f3d700b69
Oruganti Chetan Reddy <56594923+chetan-1924@users.noreply.github.com>
2022-01-09 16:53:41 +0530

    imp !!! script for donations
    From next time you don't have to open a new terminal for node donations just run
    npm run dev that is all you nedd.

--- backend/donations.js
+++ backend/donations.js
@@ -1,71 +1,79 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import fs from 'fs'
-import cors from 'cors'
-import { format } from 'date-fns'
+import express from "express";
+import bodyParser from "body-parser";
+import fs from "fs";
+import cors from "cors";
+import { format } from "date-fns";
 
 var app = express(); //include the module .this give us the capability the read the body.
 
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
+	bodyParser.urlencoded({
+		extended: true,
+	})
 );
-app.use(cors())
+app.use(cors());
 // let users = ["vineeta:test123","meenu:test123","pankaj:test123","rakesh:test123"];
 app.get("/dnations", function (request, response) {
-  var obj = JSON.parse(fs.readFileSync('./data/donations.json'));
-  console.log(obj)
-  response.status(200).json(obj);
-  const pd = new Date(Date.now())
-  console.log(format(pd, 'dd.MM.yyyy HH:mm:ss'))
-  console.log(format(new Date(Date.now()), 'dd/MM/yyyy HH:mm:ss'))
-  return 
-})
+	var obj = JSON.parse(fs.readFileSync("./backend/data/donations.json"));
+	console.log(obj);
+	response.status(200).json(obj);
+	const pd = new Date(Date.now());
+	console.log(format(pd, "dd.MM.yyyy HH:mm:ss"));
+	console.log(format(new Date(Date.now()), "dd/MM/yyyy HH:mm:ss"));
+	return;
+});
 app.post("/dnations", function (request, response) {
-  let uname = request.body.name;
-  let email = request.body.email;
-  let cat = request.body.dcat;
-  let title = request.body.dtitle;
-  let udesc = request.body.ddesc;
-//   console.log(donations)
-  if(!uname) {
-      response.status(400).json("Empty name request");
-// stop further execution in this callback
-      return;
-  }
-  if(!email) {
-      response.status(400).json("Undefined email request");
-// stop further execution in this callback
-      return;
-  }
-  if(!cat) {
-    response.status(400).json("No category found");
-// stop further execution in this callback
-    return;
-  }
-  if(!title) {
-    response.status(400).json("Donation title missing");
-// stop further execution in this callback
-    return;
-  }
-  if(!udesc) {
-    response.status(400).json("Description not found");
-// stop further execution in this callback
-    return;
-  }
-  var obj = JSON.parse(fs.readFileSync('./data/donations.json'));
-  const keyC = Object.keys(obj).length + 1
-  var pdate = new Date(Date.now())
-  obj.push({"key":keyC,"date":pdate,"name":uname,"email":email,"cat":cat,"title":title,"desc":udesc});
-  console.log(obj)
-  const jsonStr = JSON.stringify(obj);
-  fs.writeFile('./data/donations.json', jsonStr, (err) => {
-    if (err) console.log('Error writing file:', err);
-  })
+	let uname = request.body.name;
+	let email = request.body.email;
+	let cat = request.body.dcat;
+	let title = request.body.dtitle;
+	let udesc = request.body.ddesc;
+	//   console.log(donations)
+	if (!uname) {
+		response.status(400).json("Empty name request");
+		// stop further execution in this callback
+		return;
+	}
+	if (!email) {
+		response.status(400).json("Undefined email request");
+		// stop further execution in this callback
+		return;
+	}
+	if (!cat) {
+		response.status(400).json("No category found");
+		// stop further execution in this callback
+		return;
+	}
+	if (!title) {
+		response.status(400).json("Donation title missing");
+		// stop further execution in this callback
+		return;
+	}
+	if (!udesc) {
+		response.status(400).json("Description not found");
+		// stop further execution in this callback
+		return;
+	}
+	var obj = JSON.parse(fs.readFileSync("./backend/data/donations.json"));
+	const keyC = Object.keys(obj).length + 1;
+	var pdate = new Date(Date.now());
+	obj.push({
+		key: keyC,
+		date: pdate,
+		name: uname,
+		email: email,
+		cat: cat,
+		title: title,
+		desc: udesc,
+	});
+	console.log(obj);
+	const jsonStr = JSON.stringify(obj);
+	fs.writeFile("./data/donations.json", jsonStr, (err) => {
+		if (err) console.log("Error writing file:", err);
+	});
 
-  response.status(200).json("user authenticated");
-  return
+	response.status(200).json("user authenticated");
+	return;
 });
 app.listen(4000);
