a7e035cf28b38eb23790795ee9a00f543c1b5aae
Oruganti Chetan Reddy <56594923+chetan-1924@users.noreply.github.com>
2022-04-06 09:47:41 +0530

    swagger update

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -12,7 +12,6 @@ import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
 router.route("/:id/reviews").post(protect, createproductreview);
-
 router
 	.route("/:id")
 	.get(getProductById)
